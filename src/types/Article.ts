0f13e3fbb9736b88593eda62da73ae4875b156c6
AlexRatmansky <alexratmansky@yandex.ru>
2018-02-12 00:53:13 +0300

    Настраивает рендер из yaml

--- src/types/Article.ts
+++ src/types/Article.ts
@@ -1,5 +1,21 @@
 export type Article = {
-  title: string,
-  date: string,
+  title: string
+  date: string
   path: string
+};
+
+export type ert = {
+  index?: number
+  text: [string]
+  author?: string
+};
+
+export type fileData = {
+  title: string
+  month: string
+  day: string
+
+  intro: ert
+  body: [ert]
+  conclusion: ert
 };
