8aab60bb144aa267bf9b4b6b7b4bcb2e6c036cff
AlexRatmansky <alexratmansky@yandex.ru>
2018-08-21 16:44:02 +0300

    rs fix

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -1,22 +1,65 @@
 import * as express from 'express';
 const router = express.Router();
 
+import { promisify } from 'util';
 import * as fs from 'fs';
 import * as moment from 'moment';
 
 import { parseArticleFileData } from '../helpers/file';
+import { RssItem } from 'Article';
+
+const readFileAsync = promisify(fs.readFile);
 
 /* GET users listing. */
 router.get('/rss', function (req, res) {
 
   const todayDate = moment();
+
+  todayDate.set({
+    'hour': 0,
+    'minute': 0,
+    'second': 0,
+    'millisecond': 0
+  });
+
+  const completeParams: { articles: RssItem[] } = { articles: [] };
+
   const pathToFile = `./pages/${todayDate.format('MM')}/${todayDate.format('DD')}.yml`;
 
-  fs.readFile(pathToFile, 'utf8', function (err, data) {
+  readFileAsync(pathToFile, { encoding: 'utf8' })
+    .then((text) => {
+      completeParams.articles.push(formArticleData(text));
+      return readFileAsync(`./pages/${todayDate.subtract(1, 'day').format('MM')}/${todayDate.format('DD')}.yml`, { encoding: 'utf8' });
+    })
+    .then((text) => {
+      completeParams.articles.push(formArticleData(text));
+      return readFileAsync(`./pages/${todayDate.subtract(1, 'day').format('MM')}/${todayDate.format('DD')}.yml`, { encoding: 'utf8' });
+    })
+    .then((text) => {
+      completeParams.articles.push(formArticleData(text));
+      return readFileAsync(`./pages/${todayDate.subtract(1, 'day').format('MM')}/${todayDate.format('DD')}.yml`, { encoding: 'utf8' });
+    })
+    .then((text) => {
+      completeParams.articles.push(formArticleData(text));
+      return readFileAsync(`./pages/${todayDate.subtract(1, 'day').format('MM')}/${todayDate.format('DD')}.yml`, { encoding: 'utf8' });
+    })
+    .then((text) => {
+      completeParams.articles.push(formArticleData(text));
+      return readFileAsync(`./pages/${todayDate.subtract(1, 'day').format('MM')}/${todayDate.format('DD')}.yml`, { encoding: 'utf8' });
+    })
+    .then(() => {
+      res.set('Content-Type', 'text/xml');
+      res.render('feed/rss', completeParams);
+    })
+    .catch((err) => {
+      console.log('ERROR:', err);
+    });
+
+  function formArticleData(data: any) {
 
     const fileData = parseArticleFileData(data);
 
-    const renderParams = {
+    return {
       title: fileData.title || 'Empty title',
       date: todayDate.toDate().toISOString(),
       slug: `${todayDate.format('MM')}-${todayDate.format('DD')}`,
@@ -24,10 +67,7 @@ router.get('/rss', function (req, res) {
       body: fileData.body,
       conclusion: fileData.conclusion,
     };
-
-    res.set('Content-Type', 'text/xml');
-    res.render('feed/rss', renderParams);
-  });
+  }
 });
 
 export default router;
